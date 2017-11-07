4d885f6b8b5e8e99ca8b923b274de4ee3ce994ab
Renpengfei <18516221540@163.com>
2017-11-07 22:14:48 +0800

    first commit

---
+++ MyFirstGitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyFirstGitDemo
+//
+//  Created by Renpengfei on 2017/11/7.
+//  Copyright © 2017年 Renpengfei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
